431113fdc69a4aa7c869937f4cba6ebda7d18abf
Finnedorn <daniele.giuntoli@outlook.com>
2023-11-08 14:10:16 +0100

    tested all components we shared during lesson

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <!--qui va html del componente-->
+    <HeaderComponent/>
+    <main>
+        <h2>
+            {{titolo}}
+        </h2>
+    </main>
+</template>
+
+<script>
+    import HeaderComponent from  './components/HeaderComponent.vue'
+    export default {
+        name:'App',
+        components: {
+            HeaderComponent
+        },
+        data(){
+           return {
+            titolo: 'ciaooo!!',
+
+           }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style lang="css">
+
+main {
+    text-align: center;
+}
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <h2>
+            {{ titolo }}
+        </h2>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+        data(){
+            return {
+                titolo: 'sono un header :)'
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+
+h2 {
+    color: red;
+}
+
+</style>
